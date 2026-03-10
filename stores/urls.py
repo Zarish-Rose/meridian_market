04d5471ab04757059ad5dcedfc86707befc97b99
Zarish-Rose <zarishrose@icloud.com>
2026-03-10 18:13:52 +0000

    Add create store functionality

--- stores/urls.py
+++ stores/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.store_list, name='store_list'),
+    path('create/', views.create_store, name='create_store'),
 ]
--- stores/views.py
+++ stores/views.py
@@ -6,3 +6,21 @@ from .models import Store
 def store_list(request):
     stores = Store.objects.filter(owner=request.user)
     return render(request, 'stores/store_list.html', {'stores': stores})
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import StoreForm
+
+@login_required
+def create_store(request):
+    if request.method == 'POST':
+        form = StoreForm(request.POST)
+        if form.is_valid():
+            store = form.save(commit=False)
+            store.owner = request.user
+            store.save()
+            return redirect('store_list')
+    else:
+        form = StoreForm()
+
+    return render(request, 'stores/create_store.html', {'form': form})
