4f3eaf2ea283e5f1009b4011f6345b74d35c4213
Zarish-Rose <zarishrose@icloud.com>
2026-03-16 12:52:54 +0000

    Update profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='home/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('myprofile/', views.my_profile, name='my_profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,14 +3,17 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from .forms import ProfileForm
-from accounts.decorators import role_required
+from .models import Profile
+from stores.models import Store
 
 
 def index(request):
     return render(request, 'home/home.html')
 
+
 def about(request):
     return render(request, 'home/about.html')
+
 
 def register(request):
     if request.method == 'POST':
@@ -24,24 +27,28 @@ def register(request):
 
     return render(request, 'home/register.html', {'form': form})
 
-@login_required
-def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
 
 @login_required
-def edit_profile(request):
-    profile = request.user.profile
+def my_profile(request):
+    user = request.user
+    profile, _ = Profile.objects.get_or_create(user=user)
 
     if request.method == 'POST':
         form = ProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            return redirect('dashboard')
+            return redirect('my_profile')
     else:
         form = ProfileForm(instance=profile)
 
-    return render(request, 'accounts/edit_profile.html', {'form': form})
+    if profile.role == 'owner':
+        store = Store.objects.filter(owner=user).first()
+    elif profile.role in ['staff', 'manager']:
+        store = Store.objects.filter(members__user=user).distinct().first()
+    elif profile.role == 'admin':
+        store = Store.objects.first()
+    else:
+        store = None
 
-@role_required('owner')
-def shop_settings(request):
-    ...
+    context = {'store': store, 'form': form}
+    return render(request, 'accounts/my_profile.html', context)
